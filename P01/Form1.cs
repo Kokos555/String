e0590b71c7174685362f5350b4a2b0aba6c9fc29
Vít Novotný <novotnyv.03@spst.cz>
2022-03-30 17:57:03 +0200

    P01 Hotov

--- P01/Form1.cs
+++ P01/Form1.cs
@@ -16,5 +16,30 @@ namespace P01
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            string samohlasky = "aeiouyáéěíóúůý";
+            string souhlasky = "bcčdďfghjklmnpqrřsštťvwxzž";
+            int samo = 0;
+            int souh = 0;
+            int nepismen = 0;
+            text = text.ToLower();
+            foreach(char s in text)
+            {
+                if (samohlasky.Contains(s))
+                {
+                    samo++;
+                }
+                if (souhlasky.Contains(s))
+                {
+                    souh++;
+                }
+            }
+            int delka_vety = text.Length;
+            nepismen = delka_vety - (souh + samo);
+            MessageBox.Show(string.Format("Věta: ({0}) obsahuje {1} samohlásek {2} souhlásek a {3} nepárových znaků",text,samo,souh,nepismen));
+        }
     }
 }
