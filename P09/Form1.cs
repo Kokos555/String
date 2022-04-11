f13d770b3928c7eada1f6a568ac997a963acb13e
Vít Novotný <novotvit@hotmail.com>
2022-04-11 09:33:11 +0200

    P10 Hotovo

---
+++ P09/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tecka = "";
+            string zalozni = "";
+            string prvni_radek = "";
+            for (int i = 0; i < textBox1.Lines.Count(); i++)
+            {
+                string veta = textBox1.Lines[i];
+                veta = veta.Trim();
+                if (veta != "") { 
+                    prvni_radek = veta;
+                    if (checkBox1.Checked)
+                    {
+                        char pismeno = veta[0];
+                        prvni_radek = char.ToUpper(pismeno) + veta.Substring(1);
+                    }
+                    if (checkBox2.Checked) {
+                        if (veta[veta.Length - 1] != '.') {
+                            tecka = ".";
+                        }
+                    }
+                    zalozni += prvni_radek + tecka + Environment.NewLine;
+                }
+            }
+            textBox1.Text = zalozni;
+        }
+    }
+}
---
+++ P10/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string slovo = textBox1.Text;
+            string[] cele_slovo = slovo.Split(' ');
+            for (int i = 0; i< cele_slovo.Length; i++) {
+                if (cele_slovo[i].Any(char.IsDigit)) {
+                    listBox1.Items.Add(cele_slovo[i]);
+                }
+            }
+        }
+    }
+}
