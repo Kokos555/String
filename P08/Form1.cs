0679a61aacb63a8891b32b8c5727353988ddc276
Vít Novotný <novotvit@hotmail.com>
2022-04-11 08:29:42 +0200

    P08 Hotovo

--- P08/Form1.cs
+++ P08/Form1.cs
@@ -16,12 +16,16 @@ namespace P08
         {
             InitializeComponent();
         }
-        Font smallfont = new Font(,textbo1.font.name 12);
-        Font mediumfont = new Font("Arial", 20);
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Font smallfont = new Font(textBox1.Font.Name, 12);
+            Font mediumfont = new Font(textBox1.Font.Name, 20);
             string veta = textBox1.Text;
+            int pocet = 0;
+            string[] pocet_slov = veta.Split(' ');
+            int max = 0;
+            string slovo = "";
             if (radioButton1.Checked)
             {
                 label1.Font = smallfont;
@@ -38,6 +42,26 @@ namespace P08
             }else
             {
                 label1.ForeColor = Color.Black;
+            }
+            if (checkBox1.Checked) {
+                for (int i = 0; i < pocet_slov.Length; i++)
+                {
+                    if (pocet_slov[i] != "")
+                    {
+                        pocet++;
+                    }
+                }
+                MessageBox.Show(string.Format("{0} = {1} slov", veta, pocet));
+            }
+            if (checkBox2.Checked) {
+                for (int i = 0; i < pocet_slov.Length-1; i++) {
+                    int pocet1 = pocet_slov[i].Length;
+                    if (max < pocet1) {
+                        max = pocet1;
+                        slovo = pocet_slov[i];
+                    }
+                }
+                MessageBox.Show(string.Format("Nejdelsi slovo je ({0}) a ma {1} pismen", slovo, max));
             }
         }
     }
